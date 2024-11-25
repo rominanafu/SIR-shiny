6e5769a9a414632de8e6e6371615d7ea2e74148e
juanbrau.or <juanbrau.or@gmail.com>
2024-11-24 19:05:01 -0600

    [FIX] estocastico basico

--- modelo_basico_estocastico.R
+++ modelo_basico_estocastico.R
@@ -2,7 +2,7 @@
 library(data.table)
 library(ggplot2)
 
-dias <- 100
+dias <- 200
 t <- 1:dias
 
 S <- rep(0, dias)
@@ -15,6 +15,7 @@ S[1] <- N-I[1]-R[1]
 
 p_infeccion <- 0.05
 p_recuperacion <- 0.5
+p_muerte <- 0.04
 
 for( i in seq(dias-1) ) {
   infected <- sum(rbinom(S[i], 1, p_infeccion))
@@ -22,6 +23,14 @@ for( i in seq(dias-1) ) {
   S[i+1] <- S[i]-infected
   I[i+1] <- I[i]+infected-recovered
   R[i+1] <- R[i]+recovered
+  muertos_S <- sum(rbinom(S[i+1], 1, p_muerte))
+  muertos_I <- sum(rbinom(I[i+1], 1, p_muerte))
+  muertos_R <- sum(rbinom(R[i+1], 1, p_muerte))
+  S[i+1] <- S[i+1]-muertos_S
+  I[i+1] <- I[i+1]-muertos_I
+  R[i+1] <- R[i+1]-muertos_R
+  nacimientos <- muertos_S+muertos_I+muertos_R
+  S[i+1] <- S[i+1]+nacimientos
 }
 
 p <- ggplot() +
